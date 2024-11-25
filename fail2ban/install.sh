cb7ee952483c52f111dd9b0182c98a1a132ab401
Cameron Cross <cameroncros@gmail.com>
2024-11-25 12:09:19 +1100

    Fixed install script

--- fail2ban/install.sh
+++ fail2ban/install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-sudo cp discordshim.local /etc/fail2ban
-sudo cp discordshim.conf /etc/fail2ban/filters.d/
+sudo cp discordshim.local /etc/fail2ban/jail.d/
+sudo cp discordshim.conf /etc/fail2ban/filter.d/
 
 sudo systemctl restart fail2ban
-sudo fail2ban-client status discordshim
+
+sudo watch fail2ban-client status discordshim
