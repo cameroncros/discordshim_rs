085cc7fc8b70cacf4785af58cdec47fd57dbf0f9
Cameron Cross <cameroncros@gmail.com>
2026-08-11 10:18:49 +1000

    Disable stats endpoint

--- src/discord.rs
+++ src/discord.rs
@@ -98,13 +98,13 @@ pub(crate) async fn event_handler(
             tokio::spawn(run_server(context, data.server.clone()));
         }
         FullEvent::Message { new_message } => {
-            if new_message.guild_id.is_none() && new_message.content == "/stats" {
-                data.server
-                    .read()
-                    .await
-                    .send_stats(new_message.channel_id, ctx.clone())
-                    .await;
-            }
+            // if new_message.guild_id.is_none() && new_message.content == "/stats" {
+            //     data.server
+            //         .read()
+            //         .await
+            //         .send_stats(new_message.channel_id, ctx.clone())
+            //         .await;
+            // }
 
             // Process all other messages as normal.
             let _ = data
